bfc959cdd4559c4d76ca38dd2d03e49a0f25cadf
Anand Kadiwal(UST <162539@ust.com>
2026-09-23 12:34:45 +0530

    Cinemate project changes

---
+++ react/cinemate/src/routes/AllRoutes.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import { MovieList, MovieDetail, Search, PageNotFound } from '../pages'
+
+export const AllRoutes = () => {
+  return (
+    <div className="dark:bg-darkbg">
+      <Routes>
+        <Route path="/" element={<MovieList apiPath="movie/now_playing" title="Home" /> } />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/movies/popular" element={<MovieList apiPath="movie/popular" title="Popular Movies" /> } />
+        <Route path="/movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated Movies" /> } />
+        <Route path="/movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming Movies" /> } />
+        <Route path="/search" element={<Search apiPath="search/movie" title="Search Movies" /> } />
+        <Route path="*" element={<PageNotFound  title="Page Not Found" />} />
+      </Routes>
+    </div>
+  )
+}
